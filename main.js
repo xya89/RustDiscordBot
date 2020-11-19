1c593839c930915d17c78a23a22410c21260ca4b
xya89 <xiaoyang_yang@sfu.ca>
2020-11-18 17:44:30 -0800

    Add command sections

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// The following code is cited from 
+// https://www.youtube.com/watch?v=nTGtiCC3iQM&list=PLbbLC0BLaGjpyzN1rg-gK4dUqbn8eJQq4&index=2
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -9,7 +12,29 @@ client.once('ready', () => {
 })
 
 
+// basic custome commands for bot
+const prefix = '-';
 
+
+// Error checking for bot prefix
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot)
+    return;
+
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // adding commands
+    if(command === 'ping') {
+        message.channel.send('pong!')
+    }
+    else if(command === 'wiki') {
+        message.channel.send('https://rustlabs.com')
+    }
+
+
+});
 
 
 
@@ -27,5 +52,6 @@ client.once('ready', () => {
 
 
 // client token - access key to the bot
-client.login('Nzc4NzkxMDA1ODA0NTYwNDM0.X7XHpQ.I9c0CqrdQ_-ER8AeOcmzATcR9AA');
+// for safety issues, token will be changed everytime doing a git commit push
+client.login('Nzc4NzkxMDA1ODA0NTYwNDM0.X7XHpQ.06zojvunNSfsazucd3H9iTjRx9U');
 
