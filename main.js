369734eb0941053ef3db1579b65990492ceec8a5
xya89 <xiaoyang_yang@sfu.ca>
2020-11-18 17:29:51 -0800

    Add main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+// log function
+client.once('ready', () => {
+    console.log('RustBoi is online!');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// client token - access key to the bot
+client.login('Nzc4NzkxMDA1ODA0NTYwNDM0.X7XHpQ.I9c0CqrdQ_-ER8AeOcmzATcR9AA');
+
